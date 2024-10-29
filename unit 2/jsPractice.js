e3f9880de7673b2f55f4a418764495c70b34bc16
Semper-Artist <wscott035@boyslatin.net>
2024-10-29 17:40:29 +0000

    William Scott- Practice with Javascript(Notes)

---
+++ unit 2/jsPractice.js
@@ -0,0 +1,33 @@
+// two forward slashes = this is a comment
+
+// string data type
+// strings are pieces of data that are wrapped around
+//quotation marks
+"some text..."
+"84992998858"
+"@Williamscott 2949%2!e9"
+'a string with single quotes...'
+
+// Integer
+// an integer is a piece of data that represents 
+// a whole number
+3995339
+394093294
+6
+89
+
+// Boolean
+// a boolean is a piece of 
+// data that represents a true or false value
+true , 1
+false , 0
+
+// variables
+// variables are constructs that store data
+var number = 0
+
+// temporary variable= this data can change
+let image1= '';
+
+// fixed variable = cannot/ should not change.
+const text= '';
