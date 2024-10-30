186983e3d46bc6e289d5f05fb4d5565c6b970e1f
Semper-Artist <wscott035@boyslatin.net>
2024-10-30 16:47:27 +0000

    Further development in the javascript notes.

--- unit 2/jsPractice.js
+++ unit 2/jsPractice.js
@@ -1,7 +1,7 @@
 // two forward slashes = this is a comment
 
 // string data type
-// strings are pieces of data that are wrapped around
+// strings are pieces of data that are bound together by
 //quotation marks
 "some text..."
 "84992998858"
@@ -11,6 +11,7 @@
 // Integer
 // an integer is a piece of data that represents 
 // a whole number
+// even decimals
 3995339
 394093294
 6
@@ -24,10 +25,35 @@ false , 0
 
 // variables
 // variables are constructs that store data
+// types
+// we give a value a name to be associated within
+// the programs memory
+
+// universal variable/ data container
 var number = 0
 
 // temporary variable= this data can change
-let image1= '';
+let loginName= 'Kaze123';
 
 // fixed variable = cannot/ should not change.
-const text= '';
+const password= 'abceasyas123';
+
+// syntax - how we write and format our code. 
+// when we do not follow the format rules, just like in English
+// it can be hard for the computer
+// to understand what we want it to do
+
+// warnings and errors
+var number= 10;
+console.log("this is the terminal")
+
+// syntax rules for variables
+// step 1- you first have to declare that it is a variable
+let
+var
+
+// you can use either var, let, or const
+// step 2- we need to give our variable a name
+let name = 'Lamborghini'
+var carType = 'Super'
+
