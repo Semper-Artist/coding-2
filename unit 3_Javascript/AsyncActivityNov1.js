042311a5c959128df212131f3498262be7b3041e
Semper-Artist <wscott035@boyslatin.net>
2024-11-01 17:40:28 +0000

    William Scott- Async Activity for Nov 1st

---
+++ unit 3_Javascript/AsyncActivityNov1.js
@@ -0,0 +1,20 @@
+"1."
+// var is a universal container for data, making it well versed. 
+// let varifies that information can or is allowed to change.
+// const declares that data CANNOT or SHOULD NOT change. It
+// is a constant.
+
+
+"2."
+ print('2 x 2 x 1000= 4000')
+
+ 
+ 
+ "3."
+ "You would use the let variable to show time, because time will change every second or minute."
+ "Const would be used to show the current city you want to tell the weather in."
+
+ "4."
+ // I do not feel that the class should change. 
+ // The class is like a college course.
+ // If students do not lock in, they are more prone to fail.
