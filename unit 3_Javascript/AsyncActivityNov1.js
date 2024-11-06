28eb84c64a8ad6487829a4413e204e2be1d03b13
Semper-Artist <wscott035@boyslatin.net>
2024-11-06 18:06:11 +0000

    William Scott- Added the Async questions to go with the answers(To organize).

--- unit 3_Javascript/AsyncActivityNov1.js
+++ unit 3_Javascript/AsyncActivityNov1.js
@@ -1,3 +1,11 @@
+// 1. Describe the difference between var, let, and const. Your answer can be formatted as a comment or a string.
+
+// 2. Use what you've learned about data types and variables and create three variables that when multiplied together will result in the value 4000. Research the console.log() function on w3schools to output your result in the terminal.
+
+// 3. Describe a scenario where you would use the let variable and when you would use a const variable in a computer program. Your answer can be formatted as a comment or a string.
+
+// 4. If there were three things you that feel could improve the class, what would those things be and why? Your answer can be formatted as a comment or a string.
+
 "1."
 // var is a universal container for data, making it well versed. 
 // let varifies that information can or is allowed to change.
