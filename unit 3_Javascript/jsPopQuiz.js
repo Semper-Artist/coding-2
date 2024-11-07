89e3e7909df9d92347b28992ba53e1d062950a91
Semper-Artist <wscott035@boyslatin.net>
2024-11-07 18:56:40 +0000

    William Scott - Quiz on 11/07/24 complete.

---
+++ unit 3_Javascript/jsPopQuiz.js
@@ -0,0 +1,77 @@
+// 1. How do you declare a variable in JavaScript? Please write your answer 
+// using JavaScript code. 
+
+// 2. How would you declare a variable where the data WILL NOT change in JavaScript?
+// Please write your answer using JavaScript code. 
+
+
+// 3. Identify the following JavaScript operators and describe when you would use it?
+// Please write your answers using the string data type. 
+//  ===
+//  ==
+//  = 
+
+// 4. Describe a scenario where you would use a comparison operator,
+// and then provide a code example based on the scenerio you provided. 
+
+// 5. Describe a scenario where you would use a logical operator,
+// and then provide a code example based on the scenerio you provided. 
+
+// 6. Create three variables where when combined will result in the value of 36. 
+// Please use the console.log () to out put your result. 
+
+// 7. Fill in the variable that will output the appropriate result. 
+
+var x ='Please replace this text with your answer.'
+var y = x *2
+
+// Your console log should output the value 28.
+console.log(y)
+
+// 8. What does it "concatenation" mean? Please write your response as a string
+// and also provide a code example of two variables being concatenated and output the result
+// using the console.log() function. 
+
+
+// 1.
+"A variable in Javascript is declared in the following ways: var, let, and const."
+
+// 2.
+const u = '// declares when a constant variable SHOULD NOT change. It is a constant.'
+
+// 3.
+"=== refers to equivalent value and equivalent type. == refers to data being equal to. = is an assignment operator that assigns two values together"
+
+// 4. 
+"I would want to use the comparison operator whenever I want to compare two pieces of data."
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+'in this instance, text one is compared to text 2.'
+
+// 5. 
+"a logical operator could be used in order to make a logical description between two things."
+" &&| logical and"
+"|| | logical or"
+"!  |logical not"
+
+// 6.
+var a= 2
+var b= 6
+var c= 3
+
+console.log(a*b*c)  
+
+// 7.
+var x = 14
+var y = x *2
+
+console.log(y)
+
+// 8.
+"concatenation is the idea of pairing strings to other strings."
+let string1 = "Nice";
+let string2 = "Job!";
+let string3 = string1 + " " + string2;
+
+ 
