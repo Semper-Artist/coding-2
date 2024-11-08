5bccf7643fef5e86efae6239a3d40cac3b126d2a
Semper-Artist <wscott035@boyslatin.net>
2024-11-08 18:30:25 +0000

    William Scott- Completed Async activity for November 8.

---
+++ unit 3_Javascript/asyncActivityNov8.js
@@ -0,0 +1,46 @@
+// questions
+// 1. What does 'string concatenation' mean? Please write your response as a string and also provide a code example of two variables being concatenated and output the result in your terminal.
+"string concatenation is the process of pairing strings to other strings. example below. "
+let string1 = "Nice";
+let string2 = "Job!";
+let string3 = string1 + " " + string2;
+console.log(string1+string2)
+
+// 2. Please list and describe three rules for creating variable names. Please provide your answer as a string.
+"variable names must start with a letter and not a number"
+"they cannot contain spaces"
+"variable names cannot be reserved the same way const can"
+
+// 3. Define each of the following variable name formats then provide a code example of how you would write them.
+// Camel case| used to identify names 
+firstName = "William";
+lastName = "Scott";
+// Snake case| naming convention in which each word is separated by underscores
+my_var = "William"
+// Pascal case| Each word is started with a capital letter
+MyVariableName = "William"
+
+
+// 4. Find the missing variable for each of the following assignment operaor code snippets.
+// JavaScript Version let a = '?'
+let w = a *b
+print(w) // the output in the terminal should be 700
+
+let a= 350
+let b= 2
+
+
+
+
+// 5. Use the logical operators and evaluate the following variables to get the requested result.
+// which logical operator would you use to reflect that the result between the code expressions below are false. 
+// Use the console.log() function to output the result in the termial, 
+// then explain why you would use that particular logical operator using a string.
+var x= 12
+
+var y= 2
+
+var z= 12/4
+
+x <= 12 <= z < 10
+console.log(12<=2>=12/4)
