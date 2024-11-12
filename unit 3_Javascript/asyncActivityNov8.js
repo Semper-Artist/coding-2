33973b0b4e05aaa30d13638dbf0cb7867426093f
Semper-Artist <wscott035@boyslatin.net>
2024-11-12 18:40:09 +0000

    William Scott- Revised Nov 8 async activity to completion

--- unit 3_Javascript/asyncActivityNov8.js
+++ unit 3_Javascript/asyncActivityNov8.js
@@ -1,33 +1,39 @@
 // questions
 // 1. What does 'string concatenation' mean? Please write your response as a string and also provide a code example of two variables being concatenated and output the result in your terminal.
-"string concatenation is the process of pairing strings to other strings. example below. "
-let string1 = "Nice";
-let string2 = "Job!";
-let string3 = string1 + " " + string2;
+"string concatenation is the process of pairing strings to two or more strings with the + symbol. example below. "
+a = "Nice";
+b = "Job!";
+c = a + " " + b;
 console.log(string1+string2)
 
 // 2. Please list and describe three rules for creating variable names. Please provide your answer as a string.
-"variable names must start with a letter and not a number"
-"they cannot contain spaces"
-"variable names cannot be reserved the same way const can"
+'const- used to declare variables that WILL NOT change'
+'let- used to declare variables that WILL change'
+'var- universal/ default variable, can be used in any context'
+
+"1. variable names must start with a letter and not a number"
+"2. they cannot contain spaces"
+"3. variable names cannot be reserved words"
 
 // 3. Define each of the following variable name formats then provide a code example of how you would write them.
-// Camel case| used to identify names 
-firstName = "William";
-lastName = "Scott";
+// Camel case| every word after the first word is capitalized.
+var username=''
+var timeRemainingInqrtr= ''
 // Snake case| naming convention in which each word is separated by underscores
-my_var = "William"
+var user_name =''
+var time_Remaining_in_qrtr= ''
 // Pascal case| Each word is started with a capital letter
-MyVariableName = "William"
+MyGoodName = "William"
 
 
 // 4. Find the missing variable for each of the following assignment operaor code snippets.
-// JavaScript Version let a = '?'
-let w = a *b
-print(w) // the output in the terminal should be 700
+// JavaScript Version 
+let a = 350
 
-let a= 350
-let b= 2
+let b = a *2
+
+console.log(b) // the output in the terminal should be 700
+
 
 
 
@@ -40,7 +46,8 @@ var x= 12
 
 var y= 2
 
-var z= 12/4
+var z= 12/y
 
-x <= 12 <= z < 10
-console.log(12<=2>=12/4)
+console.log(!(x <= 12) && z < 10)
+// 12 is less than or equal to 12 - true
+// 6 less than 1- true
