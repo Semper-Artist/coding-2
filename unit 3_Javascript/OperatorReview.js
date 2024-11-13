185a4b007ea2b28286533785b67956d2fed603df
Semper-Artist <wscott035@boyslatin.net>
2024-11-13 18:40:47 +0000

    William Scott- Reviewed and made notes on Operators in JavaScript.

---
+++ unit 3_Javascript/OperatorReview.js
@@ -0,0 +1,44 @@
+// Arithmetic Operators - processes that involve math.
+let musicSubcriptionPlan_1 = 17.00
+let bankAcount = 230.00
+
+function withdrawMusicSub(){
+    // if money exists - withdraw money for sub
+    console.log(bankAccount-musicSubcriptionPlan_1)
+    // if no money - pause sub
+}
+
+// Assignment Operators - applying a value to a variable
+// MAIN TAKEAWAY : the single equal symbol is used for assignments
+
+var jeans = 200.00
+jeans *=.25; // 25% discount= 50.00
+var discount = .25
+var calculatedDiscount = jeans * discount
+var total =jeans - calculatedDiscount
+
+console.log(total);
+
+// Comparison Operators - analysing values to one another
+// the equal symbol has special properties in JS.\
+// single equal (=) : ONLY FOR ASSIGNMENT
+var number = 4
+// double equal (==): same as or equal to
+var a = 10
+var b = 12
+console.log(a == b)
+
+var userPassword = '1234'
+var dbPassword = '12354'
+
+console.log(userPassword == dbPassword)
+
+// triple equal (===): equal value and equal type
+var depositAmount = 400.00
+var savingsAccount = 400.00
+
+console.log(depositAmount === savingsAccount)
+
+
+
+// exclaimatin equal(!=): not equal
