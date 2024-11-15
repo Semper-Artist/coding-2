ec64fa26eca7700918af19838997dc8cf2f911e2
Semper-Artist <wscott035@boyslatin.net>
2024-11-15 14:33:29 +0000

    William Scott- Async Activity for Nov 15th completed.

---
+++ unit 3_Javascript/asyncActivityNov15.js
@@ -0,0 +1,36 @@
+// 1. A company is developing a security system that requires 2 factor authorization. 
+// This means that the system needs to verify that 2 pieces of data are correct for the person to enter the building. 
+// When someone approached the building they need to have the correct name and correct passcode to enter the building.
+// Which operator would be used here? Please provide a code example of how you would write this and output the result using the print() function for python or the console.log() function for javascript?
+
+"The comparison operator can be used for this situation by using '=='(same as or equal to)"
+var userPassword= 12467
+var dbPassword= 12467
+console.log(userPassword == dbPassword)
+// If this code is ran in the terminal, one can see that 
+// the password is true. The person could safely enter the 
+// building.
+
+
+// 2. The Arithmetic operator can help to keep track of both variables
+let EGCmachine = 10
+let EGCmachineRecieved = 4
+console.log(EGCmachine - EGCmachineRecieved)
+// It will tell that they need 6 more EGC machines
+
+"For oxygen tanks, the same operator can be used."
+let oxytankneeded = 6
+let oxytankoveracquired = 9
+console.log(oxytankoveracquired - oxytankneeded)
+// The terminal would share that they need to send back three oxytanks to keep 
+// their storage at the wanted 6.
+
+// 3. The string operator would be used when users must be able to
+// capture user data and then send it to someone else.
+" user data sending: abcdefg"
+
+// 4. 
+"= represents an assignment between two values, NOT equation."
+"&& expresses a logical and, pairing two or more things."
+"|| refers to a logical or, inserting another data, but not contrasting the initial data."
+"== double equal signs say that two values are equal to each other, or the same."
