2d563127cb9c65492accd99b8cea157cb04e5674
Semper-Artist <wscott035@boyslatin.net>
2024-11-19 18:36:37 +0000

    William Scott- Learning functions and introducing JavaScript to HTML

---
+++ unit 3_Javascript/functionPractice/functions.js
@@ -0,0 +1,26 @@
+// What is a function ?
+// A function a set of instructions that perform a specific task 
+// or job.
+
+// There are two variations of functions:
+// Built-in functions: these are instructions that are already
+// in the programming language and all you need to do is 
+// pass it data.
+// console.log() is an example of a built-in function
+name= 'william scott'
+console.log(name)
+
+// User defined functions: these are custom instructions that are
+//  written by engineers.
+
+// How do we write a function - function syntax
+// declare function
+function exampleFunction() {
+    alert("you are now conncected to the JS file.");
+    console.log('step 1.')
+    console.log('step 2.')
+    console.log('step 3.')
+}
+
+// call function
+exampleFunction();
