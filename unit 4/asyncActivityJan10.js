00b4c6dd080685b1914f278c43dea73c8657d7da
Semper-Artist <wscott035@boyslatin.net>
2025-01-10 18:51:32 +0000

    William Scott- Async work for Jan 10 2025.

---
+++ unit 4/asyncActivityJan10.js
@@ -0,0 +1,32 @@
+// 1.
+
+var groceryStore = ['Frozen corn', 'Ice cream', 'Popsicles', 'Water Ice', 'Frozen french fries','Samosa', 'Mochi', 'General tsos chicken', 'Pad thai', 'carbonara', 'Apple', 'Broccoli', 'Carrots', 'Peanuts', 'Squash']
+var index1 =['Frozen corn']
+var index2 =['Ice cream']
+var index3 =['Popsicles']
+var index4 =['Water Ice']
+var index5 =['Frozen french fries']
+var index6 =['Samosa']
+var index7 =['Mochi']
+var index8 =['General tsos chicken']
+var index9 =['Pad thai']
+var index10 =['carbonara']
+var index11 =['Apple']
+var index12 =['Broccoli']
+var index13 =['Carrots']
+var index14 =['Peanuts']
+var index15 =['Squash']
+
+console.log(groceryStore);
+
+
+// 2.
+NumberList = [1,30,39,50,293,100]
+var index1 =["1"]
+var index1 =["30"]
+var index1 =["39"]
+var index1 =["50"]
+var index1 =["100"]
+var index1 =["293"]
+
+console.log(NumberList)
