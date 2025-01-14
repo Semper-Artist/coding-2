e6deabb5542c397cdc9489779555b116b232d01f
Semper-Artist <wscott035@boyslatin.net>
2025-01-14 18:41:08 +0000

    William Scott- Completed file link work for 1/14/25. Created two files and linked them together.

---
+++ unit 4/feDev/logic.js
@@ -0,0 +1,10 @@
+function logicDevelopment(){}
+    
+
+logicDevelopment()
+
+ document.getElementById("demo").innerHTML = "This data is coming from my JS file."
+
+ console.log("JS file connected")
+ 
+
