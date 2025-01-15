a7318376f9775d6b61c10202add4fb7dc7691613
Semper-Artist <wscott035@boyslatin.net>
2025-01-15 18:42:05 +0000

    William Scott- Array notes 1/15/25

---
+++ unit 4/jsArrayMethods.js
@@ -0,0 +1,13 @@
+// JavaScript Lists are called Arrays
+
+// They are more flexible compared to other programming 
+// language list data types.
+
+// In python/ Java they have Lists, Dictionary, Set, Tuple 
+// which are all under the Array family. But these list
+// data types all have a special job or function, where as
+// other data types, including other lists.
+
+var name ='Will'
+var grovery = ['apple', 'milk', 'water']
+var generalList = [name, grocery, 132323, 2334, true]
