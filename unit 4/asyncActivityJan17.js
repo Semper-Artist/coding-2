0664c12a9cfc612c7056bb981595457004fee35d
Semper-Artist <wscott035@boyslatin.net>
2025-01-21 19:07:15 +0000

    William Scott- Reviewed and Revised Async Activity Jan 17

--- unit 4/asyncActivityJan17.js
+++ unit 4/asyncActivityJan17.js
@@ -1,26 +1,23 @@
 // 1.
-// function campingNight(item){
-   // campingSupplies = ['tent','sleeping bag','flash light','camping knife'];
-   //  campingSupplies.reverse(item);
-   // console.log('tent','sleeping bag','flash light','camping knife')
-// }
+var campingSupplies = ['tent','sleeping bag','flash light','camping knife']
+var reversed_CampingSupplies= campingSupplies.reverse()
+console.log(reversed_CampingSupplies)
+
 
 // 2.
-// var campingSupplies = ['tent','sleeping bag','flash light','camping knife'];
-// campingSupplies.push=('Med Kit')
-// console.log(campingSupplies)
+reversed_CampingSupplies.push=('Med Kit')
+console.log(reversed_CampingSupplies)
 
 // 3. 
-//const array1 = ['tent','sleeping bag','flash light','camping knife'];
-//const array2 = ['marshmellows','gram crackers','chocolate','chicken hot dogs','water']
+campingFood = ['marshmellows','gram crackers','chocolate','chicken hot dogs','water',]
 
-//const campSuppliesAndFood = array1.concat(array2)
+var combinedCampingList = reversed_CampingSupplies.concat(campingFood)
+console.log(combinedCampingList)
 
 // 4.
+combinedCampingList[1]='campfire kit'
+console.log(combinedCampingList)
 
-var campingSupplies = ['tent','sleeping bag','flash light','camping knife'];
-campingSupplies.pop('');
-console.log(campingSupplies)
  
 
 
