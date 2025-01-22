69f75fd30e28aefc904f38146ca87be8f35506b3
Semper-Artist <wscott035@boyslatin.net>
2025-01-22 18:40:14 +0000

    William Scott- Notes and walkthrough of JS and HTML Loops and While Loops.

---
+++ unit 4/whileLoops.js
@@ -0,0 +1,18 @@
+// Loops - a programming construct
+// that repeats a set of code instructions
+// over and over until a specific condition
+// is met.
+
+// While Loops- this is a special type of loop
+// that repeats code instructions over and over so long
+// as a specific condition is TRUE.
+
+function basicLoop(){
+var x =2
+while (x + 2 == 4){
+    console.log('this code will repeat forever.');
+}
+}
+function welcome(){
+    console.log('excellent you are using JS')
+}
