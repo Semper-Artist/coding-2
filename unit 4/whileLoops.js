2d9058bb89027da7aa875666c0c4246474060e11
Semper-Artist <wscott035@boyslatin.net>
2025-01-23 18:42:56 +0000

    William Scott- Traffic light Loop notes.

--- unit 4/whileLoops.js
+++ unit 4/whileLoops.js
@@ -7,12 +7,29 @@
 // that repeats code instructions over and over so long
 // as a specific condition is TRUE.
 
-function basicLoop(){
-var x =2
-while (x + 2 == 4){
-    console.log('this code will repeat forever.');
+// function basicLoop(){
+// var x = 10
+//while (x < 10){
+  //  console.log('this code will repeat forever.');
+   // value += 1
+// }
+// }
+// basicLoop()
+ // function welcome(){
+  //  console.log('excellent you are using JS')
+// }
+
+
+function trafficLight(){
+var lightColors = ['red','yellow','green'];
+   var  timer = 0;
+    while (timer < 60){
+        console.log("this light is" + lightColors[0]);
+        timer += 1;
+    if (timer % 6){
+      console.log("light is about to change");
+      console.log("this light is" + lightColors[1]);
+    }
+  }
 }
-}
-function welcome(){
-    console.log('excellent you are using JS')
-}
+trafficLight();
